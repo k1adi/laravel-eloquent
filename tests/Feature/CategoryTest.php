782f29b7c54f3d92352260f7ffbed94f00e476c6
k1adi <rizkiadinugroho3@gmail.com>
2024-06-14 15:24:58 +0700

    rename ModelTest to CategoryTest

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ModelTest extends TestCase
+class CategoryTest extends TestCase
 {
     public function testInsert()
     {

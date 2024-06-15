55332994d682aa1ff6b0f5923349f7dfa20570ca
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-06-15 10:21:27 +0700

    implementation soft delete

--- tests/Feature/VoucherTest.php
+++ tests/Feature/VoucherTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Voucher;
+use Database\Seeders\VoucherSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -21,12 +22,33 @@ class VoucherTest extends TestCase
 
     public function testInsertMultiUUID()
     {
-        
         $voucher = new Voucher();
         $voucher->name = 'Voucher Test';
         $voucher->save();
 
         $this->assertNotNull($voucher->id);
         $this->assertNotNull($voucher->voucher_code);
+    }
+
+    public function testSoftDelete()
+    {
+        $this->seed(VoucherSeeder::class);
+
+        $voucher = Voucher::where('name', '=', 'Sample Voucher')->first();
+        $voucher->delete();
+
+        $voucher = Voucher::where('name', '=', 'Sample Voucher')->first();
+        $this->assertNull($voucher);
+    }
+
+    public function testGetSoftDeleted()
+    {
+        $this->seed(VoucherSeeder::class);
+
+        $voucher = Voucher::where('name', '=', 'Sample Voucher')->first();
+        $voucher->delete();
+
+        $voucher = Voucher::withTrashed()->where('name', '=', 'Sample Voucher')->first();
+        $this->assertNotNull($voucher);
     }
 }

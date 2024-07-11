8bacf703356d92bafb8991a8fa030138abdaf663
k1adi <rizkiadinugroho3@gmail.com>
2024-07-11 11:35:16 +0700

    implement local scope

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Voucher extends Model
 {
@@ -20,5 +22,15 @@ class Voucher extends Model
     public function uniqueIds()
     {
         return [$this->primaryKey, 'voucher_code'];
+    }
+
+    public function scopeActive(Builder $builder): void
+    {
+        $builder->where('is_active', true);
+    }
+
+    public function scopeNonActive(Builder $builder): void
+    {
+        $builder->where('is_active', false);
     }
 }
--- tests/Feature/VoucherTest.php
+++ tests/Feature/VoucherTest.php
@@ -21,12 +21,25 @@ class VoucherTest extends TestCase
 
     public function testInsertMultiUUID()
     {
-        
         $voucher = new Voucher();
         $voucher->name = 'Voucher Test';
         $voucher->save();
 
         $this->assertNotNull($voucher->id);
         $this->assertNotNull($voucher->voucher_code);
+    }
+
+    public function testLocalScope()
+    {
+        $voucher = new Voucher();
+        $voucher->name = 'Voucher Test';
+        $voucher->is_active = true;
+        $voucher->save();
+
+        $total = Voucher::query()->active()->count();
+        $this->assertEquals(1, $total);
+
+        $total = Voucher::query()->nonActive()->count();
+        $this->assertEquals(0, $total);
     }
 }
